911679aaba17642d0a19c0ab93a200cc01fa4430
KP <86213869+Kanishk-Pandey@users.noreply.github.com>
2022-08-19 02:25:36 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+      calcStacks();
+    }
+
+  
+    public static void calcStacks(){
+      Scanner s=new Scanner(System.in);
+    	System.out.print("Use a function of the calculator: ");
+    	String input = s.nextLine();
+
+    	//s.close();
+    	Stack<String> Calculator=new Stack<>();
+    	while(!input.equals("!")){
+    	  if(input.equals("?")){
+    	    Stack<String> copyCalculator=Calculator;
+    	    while(copyCalculator.isEmpty()==false){
+    	      System.out.println(copyCalculator.peek());
+    	      copyCalculator.pop();
+    	    }
+    	  }
+    	  else if(input.equals("+")){
+    	    int num1=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    int num2=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    System.out.println(num1+num2);
+    	  }
+    	  
+    	  else if(input.equals("-")){
+    	    int num1=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    int num2=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    System.out.println(num2-num1);
+    	 	}
+    	 	
+    	 	else if(input.equals("*")){
+    	    int num1=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    int num2=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    System.out.println(num1*num2);
+    	  }
+    	  
+    	  else if(input.equals("/")){
+    	    int num1=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    int num2=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    System.out.println(num2/num1);
+    	  }
+    	  
+    	  else if(input.equals("%")){
+    	    int num1=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    int num2=Integer.parseInt(Calculator.peek());
+    	    Calculator.pop();
+    	    System.out.println(num2%num1);
+    	  }
+    	  
+    	  else{
+    	    Calculator.push(input);
+    	  }
+    	  System.out.print("Use a function of the calculator: ");
+        input = s.nextLine();
+      }
+    }
+  }
